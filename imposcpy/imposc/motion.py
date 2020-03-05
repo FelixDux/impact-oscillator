7b8e2ef1c317d81b2d4a6e4580784503704d5717
Felix Dux <felixjdux@gmail.com>
2020-03-05 22:14:11 +0000

    motion between impacts

---
+++ imposcpy/imposc/motion.py
@@ -0,0 +1,120 @@
+from math import cos, sin, pi
+from dataclasses import dataclass
+from typing import List
+
+from .parameters import SystemParameters
+
+def phi(t: float, omega: float) -> float:
+    """
+    For a given time `t` returns the phase relative to the forcing period 2 pi /`omega`
+    """
+    if omega == 0:
+        return t
+    else:
+        return t % 2.0 * pi / omega
+
+@dataclass
+class ImpactPoint:
+    """
+    A point on the impact surface
+    """
+    phi: float
+    v: float
+
+@dataclass
+class StateOfMotion:
+    """ State and phase variables for the motion between impacts """
+    x: float  # displacement
+    v: float  # velocity = dx/dt
+    t: float  # time
+
+    def point_from_state(self, omega: float) -> ImpactPoint:
+        return ImpactPoint(phi=phi(self.t, omega), v=self.v)
+
+
+class MotionBetweenImpacts:
+    """ Coefficients and methods for time evolution of the system from one impact to the next """
+
+    def __init__(self, parameters: SystemParameters, point: ImpactPoint, recording = False):
+        self._parameters = parameters
+        self._gamma = parameters.gamma()
+        self._point: ImpactPoint = None
+        self._cos_coeff = 0.0
+        self._sin_coeff = 0.0
+        self._steps = []
+        self._recording = recording
+
+        self.set_impact(point)
+
+    def set_impact(self, point: ImpactPoint) -> None:
+        self._point = point
+        self._cos_coeff = self._parameters.sigma - self._gamma * cos(self._parameters.omega * point.phi) 
+        self._sin_coeff = -self._parameters.r * point.v + self._parameters.omega * self._gamma * 
+            sin(self._parameters.omega * point.phi)
+
+    @property
+    def recording(self) -> bool:
+        return self._recording
+
+    @recording.setter
+    def recording(self, value: bool):
+        if value != self._recording:
+            self._recording = value
+            self._steps.clear()
+
+    @property
+    def gamma(self) -> float:
+        """ the coefficient of the forcing term of the displacement """
+        return self._gamma
+
+    @property
+    def cos_coeff(self) -> float:
+        """ the coefficient of the cosine term of the displacement """
+        return self._cos_coeff
+
+    @property
+    def sin_coeff(self) -> float:
+        """ the coefficient of the sine term of the displacement """
+        return self._sin_coeff
+
+    @property
+    def steps(self) -> List[StateOfMotion]:
+        return self._steps
+
+    def motion_at_time(self, t: float) -> StateOfMotion:
+        lamda = t - self._point.phi
+        sin_lamda = sin(lamda)
+        cos_lamda = cos(lamda)
+
+        return StateOfMotion(t=t,
+            x=self._cos_coeff * cos_lamda + self._sin_coeff * sin_lamda + self._gamma * cos(self._parameters.omega * t),
+            v=self._sin_coeff * cos_lamda - self._cos_coeff * sin_lamda - self._parameters.omega * self._gamma * 
+            sin(self._parameters.omega * t))
+
+    def next_impact(self, step_size = 0.1, limit = 0.001) -> ImpactPoint:
+
+        t = self._point.phi
+
+        penetrating = False
+
+        while abs(step_size) > limit:
+            t += step_size
+
+            state = self.motion_at_time(t)
+
+            if state.x > self._parameters.sigma:
+                if not penetrating:
+                    step_size *= -0.5
+
+                penetrating = True
+
+            else:
+                if penetrating:
+                    step_size *= -0.5
+
+                penetrating = False
+
+                if self.recording:
+                    self._steps.append(state)
+
+        return ImpactPoint(phi=phi(t, self._parameters.omega), state.v)
--- imposcpy/imposc/parameters.py
+++ imposcpy/imposc/parameters.py
@@ -1,22 +1,7 @@
-from math import pi, cos, sin
+from math import pow
 from dataclasses import dataclass
 
 SMALL = 0.001
-
-def phi(t: float, omega: float) -> float:
-    """
-    For a given time `t` returns the phase relative to the forcing period 2 pi /`omega`
-    """
-    if omega == 0:
-        return t
-    else:
-        return t % 2.0 * pi / omega
-
-def gamma(omega: float) -> float:
-    if abs(omega) == 1:
-        return 1
-    else:
-        return 1.0/(1.0-:math.pow(omega, 2))
 
 
 @dataclass
@@ -28,26 +13,8 @@ class SystemParameters:
     r: float      # the coefficient of restitution at an impact
     sigma: float  # the offset from the centre of motion at which an impact occurs
 
-
-@dataclass
-class ImpactPoint:
-    """
-    A point on the impact surface
-    """
-    phi: float
-    v: float
-
-
-class EvolutionCoefficients:
-    """ Coefficients for time evolution of the system from one impact to the next """
-
-    def __init__(self, parameters: SystemParameters, point: ImpactPoint):
-        self._parameters = parameters
-        self._gamma = gamma(parameters.omega)  # the coefficient of the forcing term of the displacement
-        self._cos_coeff = parameters.sigma - result.gamma * cos(parameters.omega * point.phi)
-        self._sin_coeff = -parameters.r * point.v + parameters.omega * result.gamma * sin(parameters.omega * point.phi)
-
-
-
-if __name__ == "__main__":
-    print(phi(3, pi))
+    def gamma(self) -> float:
+        if abs(self.omega) == 1:
+            return 1
+        else:
+            return 1.0/(1.0-pow(self.omega, 2))
