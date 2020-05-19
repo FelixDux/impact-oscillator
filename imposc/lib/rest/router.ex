970a6b781564c63a9d55cc51c94b605252af62f2
Felix Dux <felixjdux@gmail.com>
2020-05-19 13:09:44 +0100

    REST api returning images

--- imposc/lib/rest/router.ex
+++ imposc/lib/rest/router.ex
@@ -12,12 +12,28 @@ defmodule Imposc.Router do
 
   get "/info/:action" do
     if Map.has_key?(actions() |> Map.new(), action) do
-      # conn
-      # |> put_resp_content_type("application/json")
-      # |> send_resp(200, JSON.encode!(action_info(action)))
       action_info(action) |> send_json(conn)
     else
       conn |> report_unmatched("/info/#{action}")
+    end
+  end
+
+  get "/images/:image" do
+    image_type = image |> Path.extname() |> String.replace(".", "")
+    content_type = "image/#{image_type}"
+
+    case ImageCache.cache_path(%ImageCache{}) do
+      {:ok, image_dir} ->
+        (fn ->
+           file_path = [image_dir, Path.basename(image)] |> Path.join()
+
+           conn
+           |> put_resp_content_type(content_type)
+           |> send_resp(200, File.read!(file_path))
+         end).()
+
+      _ ->
+        conn |> report_unmatched("/info/#{image}")
     end
   end
 
@@ -51,5 +67,15 @@ defmodule Imposc.Router do
 
   defp report_unmatched(conn, endpoint) do
     send_resp(conn, 404, "Unregonised endpoint: #{endpoint}")
+  end
+
+  defp path_to_endpoint(p) do
+    p
+    |> Path.dirname()
+    |> Path.split()
+    |> Enum.reverse()
+    |> (&with([h | _] <- &1, do: h)).()
+    |> (&([&1] ++ [Path.basename(p)])).()
+    |> Path.join()
   end
 end
